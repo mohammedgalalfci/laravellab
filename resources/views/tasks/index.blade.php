5ffaf10c547b391930c26b6703634721230e7370
mohammedgalalfci <mohammedgalalfci@gmail.com>
2022-01-31 01:54:56 -0500

    create seeder,factory and pagnitation added

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -34,6 +34,15 @@
                 @endforeach
             </tbody>
         </table>
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+            </ul>
+        </nav>
         <script>
             const del=document.getElementById('del');
             del.addEventListener('click',function(e){
