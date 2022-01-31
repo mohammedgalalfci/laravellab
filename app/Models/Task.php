6559f15ed78ba2a8def755225a485fc5c092f14d
mohammedgalalfci <mohammedgalalfci@gmail.com>
2022-01-31 11:42:19 -0500

    slug completed

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,14 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Cviebrock\EloquentSluggable\Sluggable;
 class Task extends Model
 {
+    use Sluggable;
     use HasFactory;
+
     protected $fillable = [
         'title',
         'user_id',
     ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 
     public function user()
     {
--- database/migrations/2022_01_30_082746_create_tasks_table.php
+++ database/migrations/2022_01_30_082746_create_tasks_table.php
@@ -16,6 +16,7 @@ class CreateTasksTable extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('slug');
             $table->foreignId('user_id')->nullable()->constrained();
             $table->timestamps();
         });
